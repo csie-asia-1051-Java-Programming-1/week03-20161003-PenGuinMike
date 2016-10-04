1d8ae8898b606aba5a510949f97f0b0de8a2210f
PenGuinMike <104021074@live.asia.edu.tw>
2016-10-04 23:13:04 +0800

    hw1
    不知道那個不包含0是甚麼意思

--- src/hw/hw01_104021074.java
+++ src/hw/hw01_104021074.java
@@ -2,11 +2,24 @@ package hw;
 /*
  * 輸入一個正整數值(不含 0)，傳回該數值是幾位數(# ofdigits)。
  */
+	import 	java.util.Scanner;
 public class hw01_104021074 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			Scanner scn = new Scanner(System.in);
+			System.out.print("請輸入數字:");
+			int  a,	x,	t = 0;
+			x = scn.nextInt();
+			a = x;
+			while(a>0){
+					a = a/10;
+					t++;
+			}System.out.println("該數值是"+t+"位數");
+				
+			
 
+			
 	}
 
 }
